1793c7e888f1b06558fe3bcbe3b561a5df371df8
EMRE ÖKSÜZ <emreoksuz43978@gmail.com>
2024-09-20 17:03:35 +0300

    UI

---
+++ app/src/main/java/com/emreoksuz/movieapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.emreoksuz.movieapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonLogin;
+    Button buttonSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        buttonLogin=findViewById(R.id.buttonLogin);
+        buttonSignup=findViewById(R.id.buttonSignUp);
+
+        buttonLogin.setBackgroundColor(Color.parseColor("#FF8C00"));
+        buttonSignup.setBackgroundColor(Color.parseColor("#FF8C00"));
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
